c68935b03ae89c269ef3b78421c957149a0c23fa
Aric Riggs <ariggs1226@gmail.com>
2022-01-27 12:42:12 -0600

    finished the base Documents, next I am going to pretty up the CSS.

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 let friends = ['Russ', 'Mindy', 'David', 'Nikki', 'Hunter'];
 
-loopOuter:
+// loopOuter:
 for (let i = 0; i < friends.length; i++) {
     const name = friends[i];
     // let line = 99;
     for (let line = 99; line > 0; line--) {
-        if (line >= 2) {
+        if (line > 2) {
             console.log(line + ' lines of code in the file, ' + line + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + [line - 1] + ' lines of code in the file');
-        } else {
-            console.log('1 line of code in the file, 1 line of code; ' + name +' strikes one out, clears it all out, no more lines of code in the file!');
-            continue loopOuter;
+        } else if(line == 2){
+            console.log(line + ' lines of code in the file, ' + line + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + [line - 1] + ' line of code in the file');
+        }  else {
+            console.log(line + ' line of code in the file, ' + line + ' line of code; ' + name +' strikes one out, clears it all out, no more lines of code in the file!');
+            // continue loopOuter;
         }
     }
  }
