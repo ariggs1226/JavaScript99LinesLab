d4e9082dfa8642c0b287fd110ca841360ad25a59
Aric Riggs <ariggs1226@gmail.com>
2022-01-27 15:50:29 -0600

    Tweaked after walkthrough

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ for (let i = 0; i < friends.length; i++) {
     console.log(name + ':')
     for (let line = 99; line > 0; line--) {
         if (line > 2) {
-            console.log(line + ' lines of code in the file, ' + line + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + [line - 1] + ' lines of code in the file');
+            console.log(line + ' lines of code in the file, ' + line + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + (line - 1) + ' lines of code in the file');
         } else if(line == 2){
-            console.log(line + ' lines of code in the file, ' + line + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + [line - 1] + ' line of code in the file');
+            console.log(line + ' lines of code in the file, ' + line + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + (line - 1) + ' line of code in the file');
         } else {
             console.log(line + ' line of code in the file, ' + line + ' line of code; ' + name +' strikes one out, clears it all out, no more lines of code in the file!');
         }
